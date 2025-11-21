7385d9f085616334b301c5fd8fe804cdebacf0d7
coni-cp <c.parraguezs@duocuc.cl>
2025-11-21 12:07:03 -0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -7,8 +7,8 @@
 function custom_woo_scripts() {
     if (is_shop() || is_product_category() || is_product_tag()) {
         wp_enqueue_script(
-            'custom-woo-filters',
-            get_template_directory_uri() . '/js/script-tienda.js',
+            'script-tienda',
+            get_stylesheet_directory_uri() . '/assets/js/script-tienda.js',
             array('jquery'),
             '1.0.0',
             true
