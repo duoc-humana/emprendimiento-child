f7e6204a5f874672646ca120d82d0fe46b7f1306
Sayrinix <sa.luciano@duocuc.cl>
2025-11-22 23:29:32 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+
+<main id="primary" class="site-main">
+    <?php
+    if ( have_posts() ) {
+        while ( have_posts() ) {
+            the_post();
+            the_content(); // Aquí se imprime el contenido de la página (incluye el bloque del carrito)
+        }
+    }
+    ?>
+</main>
+
+<?php get_footer(); ?>
