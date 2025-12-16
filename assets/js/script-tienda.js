74e460273f5386814d6e05ecc2c0f7fb68ae260d
Sayrinix <sa.luciano@duocuc.cl>
2025-12-15 21:05:33 -0300

    a

--- assets/js/script-tienda.js
+++ assets/js/script-tienda.js
@@ -1,6 +1,6 @@
 //Links activos
 document.addEventListener("DOMContentLoaded", function () {
-    const links = document.querySelectorAll(".navbar-nav li");
+    const links = document.querySelectorAll(".menu-item");
     const currentUrl = window.location.href;
 
     links.forEach(link => {
